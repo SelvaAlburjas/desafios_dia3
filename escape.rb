8504c0491056ae486e86faa151da81b35983719f
Selva <selvadianett@gmail.com>
2022-05-03 01:03:58 -0400

    Agregando Enunciado

--- escape.rb
+++ escape.rb
@@ -1,3 +1,12 @@
+=begin
+Se pide crear el programa escape.rb donde el usuario ingrese la gravedad y el radio, y como
+resultado obtenga la velocidad de escape (ocupar la fórmula).
+𝑉𝑒 = raiz cuadrada(2𝑔𝑟)
+
+○ 𝑔 = 9. 8 𝑚𝑡𝑠/𝑠𝑒𝑔2
+○ 𝑟 = 6371 𝑘𝑚
+=end
+
 gra = ARGV[0].to_f # 9.8
 rad = ARGV[1].to_i # 6371
  
